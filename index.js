4fe624f61887e5c69ef598b09dd0cdd96a7385eb
Matteo Collina <hello@matteocollina.com>
2018-02-10 15:02:11 +0100

    initial implementation without any customizable part

---
+++ index.js
@@ -0,0 +1,24 @@
+'use strict'
+
+const From = require('fastify-reply-from')
+
+module.exports = async function (fastify, opts) {
+  if (!opts.upstream) {
+    throw new Error('upstream must be specified')
+  }
+
+  fastify.register(From, {
+    base: opts.upstream
+  })
+
+  fastify.addContentTypeParser('*', function (req, done) {
+    done(null, req)
+  })
+
+  fastify.all('/', reply)
+  fastify.all('/*', reply)
+
+  function reply (request, reply) {
+    reply.from()
+  }
+}
---
+++ test.js
@@ -0,0 +1,52 @@
+'use strict'
+
+const t = require('tap')
+const test = t.test
+const Fastify = require('fastify')
+const proxy = require('.')
+const got = require('got')
+
+async function start () {
+  const origin = Fastify()
+  origin.get('/', async (request, reply) => {
+    return 'this is root'
+  })
+
+  origin.get('/a', async (request, reply) => {
+    return 'this is a'
+  })
+
+  await origin.listen(0)
+
+  t.tearDown(origin.close.bind(origin))
+
+  test('basic proxy', async (t) => {
+    const server = Fastify()
+    server.register(proxy, {
+      upstream: `http://localhost:${origin.server.address().port}`
+    })
+
+    await server.listen(0)
+    t.tearDown(server.close.bind(server))
+
+    const resultRoot = await got(`http://localhost:${server.server.address().port}`)
+    t.equal(resultRoot.body, 'this is root')
+
+    const resultA = await got(`http://localhost:${server.server.address().port}/a`)
+    t.equal(resultA.body, 'this is a')
+  })
+
+  test('no upstream will throw', async (t) => {
+    const server = Fastify()
+    server.register(proxy)
+    try {
+      await server.ready()
+    } catch (err) {
+      t.equal(err.message, 'upstream must be specified')
+      return
+    }
+    t.fail()
+  })
+}
+
+start()
